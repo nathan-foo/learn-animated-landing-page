f92c5e3391563e845f9f51de5bb84cdba1af0818
Nathan Foo <ntfoo27@outlook.com>
2024-10-02 22:28:29 -0500

    Develop loader animation with gsap and anime.js

--- client-next/src/app/page.js
+++ client-next/src/app/page.js
@@ -1,6 +1,101 @@
-import styles from "./styles.css";
+"use client";
+
+import "./styles.css";
+import { gsap } from "gsap";
+import anime from "animejs";
+import { useEffect, useLayoutEffect } from "react";
 
 export default function Home() {
+  const useIsomorphicLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect;
+
+  useIsomorphicLayoutEffect(() => {
+    function startLoader() {
+      let counterElement = document.querySelector(".count p");
+      let currentValue = 0;
+  
+      function updateCounter() {
+          if (currentValue < 100) {
+              let increment = Math.floor(Math.random() * 10) + 1;
+              currentValue = Math.min(currentValue + increment, 100);
+              counterElement.textContent = currentValue;
+  
+              let delay = Math.floor(Math.random() * 200) + 25;
+              setTimeout(updateCounter, delay);
+          }
+      }
+  
+      updateCounter();
+    }
+  
+    startLoader();
+    gsap.to(".count", { opacity: 0, delay: 3.5, duration: 0.5 });
+
+    let textWrapper = document.querySelector(".ml16");
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  
+    console.log(textWrapper);
+
+    anime.timeline({ loop: false })
+      .add({
+        targets: '.ml16 .letter',
+        translateY: [-100, 0],
+        easing: "easeOutExpo",
+        duration: 1500,
+        delay: (el, i) => 30 * i
+      })
+      .add({
+        targets: '.ml16 .letter',
+        translateY: [0, 100],
+        easing: "easeOutExpo",
+        duration: 3000,
+        delay: (el, i) => 2000 + 30 * i
+      });
+
+      gsap.to(".pre-loader", {
+        scale: 0.5,
+        ease: "power4.inOut",
+        duration: 2,
+        delay: 3,
+      });
+
+      gsap.to(".loader", {
+        height: 0,
+        ease: "power4.inOut",
+        duration: 1.5,
+        delay: 3.75,
+      });
+
+      gsap.to(".loader-bg", {
+        height: 0,
+        ease: "power4.inOut",
+        duration: 1.5,
+        delay: 4,
+      });
+
+      gsap.to(".loader-2", {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+        ease: "power4.inOut",
+        duration: 1.5,
+        delay: 3.5,
+      });
+
+      gsap.to(".header h1", {
+        y: -200,
+        ease: "power4.inOut",
+        duration: 1.5,
+        delay: 3.75,
+        stagger: 0.05,
+      });
+
+      gsap.to(".img", {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0 100%)",
+        ease: "power4.inOut",
+        duration: 1.5,
+        delay: 4.5,
+        stagger: 0.25,
+      });
+  }, []);
+    
   return (
     <body>
       <div className="container">
@@ -10,7 +105,7 @@ export default function Home() {
         </div>
         <div className="loader-content">
           <div className="count"><p>0</p></div>
-          <div className="copy"><p className="ml16"></p>New Reality</div>
+          <div className="copy ml16"><p className="ml16">New Reality</p></div>
         </div>
         <div className="loader-2"></div>
       </div>
